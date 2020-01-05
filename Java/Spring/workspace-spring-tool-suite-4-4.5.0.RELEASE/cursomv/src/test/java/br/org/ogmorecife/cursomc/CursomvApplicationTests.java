5bce329bb6c6501c0a94cf5c810c5d14ec7af506
Edcleyson Souza <edcleyson.souza@gmail.com>
2020-01-05 10:58:14 -0300

    Created Project

---
+++ Java/Spring/workspace-spring-tool-suite-4-4.5.0.RELEASE/cursomv/src/test/java/br/org/ogmorecife/cursomc/CursomvApplicationTests.java
@@ -0,0 +1,13 @@
+package br.org.ogmorecife.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
