b64a1788a20028f8227746eece03fa6018d701e4
Edcleyson Souza <edcleyson.souza@gmail.com>
2020-01-19 15:36:39 -0300

    Created class Produto and association ManyToMany
    Created class Produto and association ManyToMany to class Categoria
    and also code to instantiate the classes.

--- Java/Spring/workspace-spring-tool-suite-4-4.5.0.RELEASE/cursomv/src/main/java/br/org/ogmorecife/cursomc/CursomvApplication.java
+++ Java/Spring/workspace-spring-tool-suite-4-4.5.0.RELEASE/cursomv/src/main/java/br/org/ogmorecife/cursomc/CursomvApplication.java
@@ -10,7 +10,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import br.org.ogmorecife.cursomc.domain.Categoria;
+import br.org.ogmorecife.cursomc.domain.Produto;
 import br.org.ogmorecife.cursomc.repositories.CategoriaRepository;
+import br.org.ogmorecife.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CursomvApplication implements CommandLineRunner {
@@ -18,15 +20,15 @@ public class CursomvApplication implements CommandLineRunner {
 	@Autowired
 	private CategoriaRepository categoriaRepository;
 	
+	@Autowired
+	private ProdutoRepository produtoRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomvApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		// TODO Auto-generated method stub
-		Categoria cat1 = new Categoria (null,"Informática");
-		Categoria cat2 = new Categoria (null,"Escritório");
 		
 		/*
 		Another way to implements the inserts into to the database.
@@ -40,7 +42,24 @@ public class CursomvApplication implements CommandLineRunner {
 		List<Categoria> lista = Arrays.asList(cat1, cat2);		
 		categoriaRepository.saveAll(lista);*/
 		
+		// TODO Auto-generated method stub
+		Categoria cat1 = new Categoria (null,"Informática");
+		Categoria cat2 = new Categoria (null,"Escritório");
+		
+		Produto p1 = new Produto(null,"Computador",2000.00);
+		Produto p2 = new Produto(null,"Impressora",800.00);
+		Produto p3 = new Produto(null,"Mouse",80.00);
+		
+		cat1.setProdutos(Arrays.asList(p1,p2, p3));
+		cat2.setProdutos(Arrays.asList(p2));
+		
+		p1.setCategorias(Arrays.asList(cat1));
+		p2.setCategorias(Arrays.asList(cat1,cat2));
+		p3.setCategorias(Arrays.asList(cat1));
+		
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));		
+		
 	}
 
 }
