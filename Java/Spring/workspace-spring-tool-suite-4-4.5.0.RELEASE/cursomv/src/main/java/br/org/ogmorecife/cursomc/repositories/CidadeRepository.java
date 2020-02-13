7173f719a8fe65b64f4cd485f3d3aeecd00a3500
Edcleyson Souza <edcleyson.souza@gmail.com>
2020-02-13 11:58:54 -0300

    Created the domain classes Cidade and Estado
    Both the classes above were created and their respective Repositories
    classes.

---
+++ Java/Spring/workspace-spring-tool-suite-4-4.5.0.RELEASE/cursomv/src/main/java/br/org/ogmorecife/cursomc/repositories/CidadeRepository.java
@@ -0,0 +1,11 @@
+package br.org.ogmorecife.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.org.ogmorecife.cursomc.domain.Cidade;
+
+@Repository
+public interface CidadeRepository extends JpaRepository<Cidade, Integer> {
+	
+}
